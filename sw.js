0e6375f4a8b813a3ea8c24141862ffaceaa45713
Parul <parulgangwal1908@gmail.com>
2025-12-02 11:52:28 +0530

    Added scripts for firebase use

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB-Ep3yEAzFBlqOVGOxhjbmjwlSH0Xx5qU",
-    authDomain: "namo-namkeen-app.firebaseapp.com",
-    projectId: "namo-namkeen-app",
-    storageBucket: "namo-namkeen-app.firebasestorage.app",
-    messagingSenderId: "154786466552",
-    appId: "1:154786466552:web:9be55b7b599806f536490d"
+  apiKey: "AIzaSyB-Ep3yEAzFBlqOVGOxhjbmjwlSH0Xx5qU",
+  authDomain: "namo-namkeen-app.firebaseapp.com",
+  projectId: "namo-namkeen-app",
+  storageBucket: "namo-namkeen-app.firebasestorage.app",
+  messagingSenderId: "154786466552",
+  appId: "1:154786466552:web:9be55b7b599806f536490d"
 });
 
 const messaging = firebase.messaging();
 
 // Handle Background Notifications
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage(function (payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
@@ -26,7 +26,7 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-const CACHE_NAME = 'namo-v12';
+const CACHE_NAME = 'namo-v13';
 const urlsToCache = [
   '/',
   '/index.html',
