b115be06a242adc5ff9a670de430a1a289c8049a
Parul <parulgangwal1908@gmail.com>
2025-12-03 23:41:21 +0530

    feat: Update service worker cache version to v24

--- sw.js
+++ sw.js
@@ -55,7 +55,7 @@ self.addEventListener('notificationclick', function(event) {
   );
 });
 
-const CACHE_NAME = 'namo-v23'; // Increment Version
+const CACHE_NAME = 'namo-v24'; // Increment Version
 const urlsToCache = [
   '/',
   '/index.html',
